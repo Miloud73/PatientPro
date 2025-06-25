2a1f65cb59f57ea84944f1fa3ce90f11c3566353
Miloud Bendjedda <111144391+Miloud73@users.noreply.github.com>
2025-06-25 00:08:59 +0000

    fix some issue

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,4 +1,5 @@
-import * as sdk from 'node-appwrite'
+import 'dotenv/config';
+import * as sdk from 'node-appwrite';
 
 export const {
     PROJECT_ID,
@@ -7,16 +8,19 @@ export const {
     PATIENT_COLLECTION_ID,
     DOCTOR_COLLECTION_ID,
     APPOINTMENT_COLLECTION_ID,
-    NEXT_PUBLIC_BUCKET_ID: BUCKET_ID ,
-    NEXT_PUBLIC_ENDPOINT: ENDPOINT ,
+    BUCKET_ID,
+    ENDPOINT,
 } = process.env;
 
-const client = new sdk.Client();
+// console.log('Config loaded:', {
+//   PROJECT_ID: PROJECT_ID ? 'LOADED' : 'MISSING',
+//   API_KEY: API_KEY ? 'LOADED' : 'MISSING'
+// });
 
-client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
+const client = new sdk.Client();
+client.setEndpoint('https://nyc.cloud.appwrite.io/v1').setProject(PROJECT_ID!).setKey(API_KEY!);
 
 export const databases = new sdk.Databases(client);
 export const storage = new sdk.Storage(client);
 export const messaging = new sdk.Messaging(client);
 export const users = new sdk.Users(client);
-
